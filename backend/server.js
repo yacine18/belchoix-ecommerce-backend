82a70748d18d9082342c739c0fc8dabbc886ebae
unknown <hilali.yacin@gmail.com>
2021-06-13 15:58:35 +0100

    beclhoix-ecommerce

--- backend/server.js
+++ backend/server.js
@@ -34,12 +34,12 @@ app.use('/api/users', userRouter)
 
 // app.use(express.static('frontend/build'));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-// });
-// app.use((err, req, res, next) => {
-//     res.status(500).send({ message: err.message })
-// })
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+});
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message })
+})
 
 //products router
 app.use('/api/products', productRouter)
