43d8758cd68adea420058b713819d2bc0f675fd4
unknown <hilali.yacin@gmail.com>
2021-06-13 15:18:52 +0100

    belchoix-ecommerce

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,11 @@ app.use(express.urlencoded({ extended: true }))
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use(express.static(path.join(__dirname, '/frontend/build')))
 app.get('*', (req,res)=> res.sendFile(path.join(__dirname, '/frontend/build/index.html')))
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message })
+})
 
 // ROUTES //
 
