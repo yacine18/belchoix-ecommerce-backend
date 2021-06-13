77f1f5555c62ef105b4b0b345da23f398ab2034a
unknown <hilali.yacin@gmail.com>
2021-06-13 16:27:41 +0100

    belchoix-ecommerce

--- backend/server.js
+++ backend/server.js
@@ -25,11 +25,10 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('frontend/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    });
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.get('*', (req, res) =>
+        res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+    );
 }
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message })
