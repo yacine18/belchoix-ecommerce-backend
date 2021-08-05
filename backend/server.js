ed4568eaeabb5d30f5f29dce55c61795302cbe6d
unknown <hilali.yacin@gmail.com>
2021-08-05 14:50:25 +0100

    deploy-heroku

--- backend/server.js
+++ backend/server.js
@@ -1,50 +1,46 @@
-import express from 'express';
-import path from 'path'
-import dotenv from 'dotenv';
-import './database.js'
-import cors from 'cors';
-import database from './database.js';
-import userRouter from './routers/users.router.js';
-import productRouter from './routers/products.router.js';
-import orderRouter from './routers/orders.router.js'
-
-
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import "./database.js";
+import cors from "cors";
+import database from "./database.js";
+import userRouter from "./routers/users.router.js";
+import productRouter from "./routers/products.router.js";
+import orderRouter from "./routers/orders.router.js";
 
 const app = express();
 
 //Connecting DB
-database()
+database();
 
 //middleware
 dotenv.config();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
-
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
 
 // ROUTES //
 
 // users router
-app.use('/api/users', userRouter)
-
-
+app.use("/api/users", userRouter);
 
 //products router
-app.use('/api/products', productRouter)
+app.use("/api/products", productRouter);
 
 //orders routers
-app.use('/api/orders', orderRouter)
+app.use("/api/orders", orderRouter);
 
 //paypal clientID
-app.get('/api/config/paypal', (req,res) => {
-    res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
-})
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
 
-
-
-const port = process.env.PORT || 5000
-app.listen(port, () => console.log('Server is running'))
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("Server is running"));
