f44fe3da7a186d37d9212971a84255a61b527752
unknown <hilali.yacin@gmail.com>
2021-08-06 16:06:27 +0100

    belchoix-backend

--- server.js
+++ server.js
@@ -21,10 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-// app.use(express.static(path.join(__dirname, "/frontend/build")));
-// app.get("*", (req, res) =>
-//   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-// );
+
 
 // ROUTES //
 
